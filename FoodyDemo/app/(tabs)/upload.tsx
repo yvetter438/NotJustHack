44db6b5508384467cee9a77c1dcea680ca7f65fd
yvetter438 <yannick.vetter@outlook.com>
2024-08-25 02:49:47 -0500

    imagepicker

--- FoodyDemo/app/(tabs)/upload.tsx
+++ FoodyDemo/app/(tabs)/upload.tsx
@@ -1,19 +1,45 @@
 import { View, Text, Image, StyleSheet, TextInput, Pressable } from "react-native";
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 
 export default function Upload() {
     const [caption, setCaption] = useState('');
+    const [image, setImage] = useState<string | null>(null);
+
+    useEffect(() => {
+        if (!image) {
+            pickImage();
+        }
+    }, [image])
+    const pickImage = async () => {
+        // No permissions request is necessary for launching the image library
+        let result = await ImagePicker.launchImageLibraryAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images,
+          allowsEditing: true,
+          aspect: [4, 3],
+          quality: 1,
+        });
+    
+        console.log(result);
+    
+        if (!result.canceled) {
+          setImage(result.assets[0].uri);
+        }
+      };
 
     return (
         <View style={styles.container}>
             <View style={styles.topContent}>
-                <Image
+                {image ? (<Image
                     style={styles.image}
-                    source={{
-                        uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/images/1.jpg',
-                    }}
-                />
-                <Text style={styles.text}>Change</Text>
+                    source={{uri: image,}}
+                /> 
+                ) : (
+                    <View/>
+                )}
+                <Text 
+                    onPress={pickImage}
+                    style={styles.text}>Change</Text>
 
                 <TextInput 
                     value={caption}
